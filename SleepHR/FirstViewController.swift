d18a8fa59974e8728f520f3b9ff2a366c8ab07bc
yanxin <yanxin@cs.dartmouth.edu>
2018-04-21 11:10:43 -0400

    Initial Commit

---
+++ SleepHR/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SleepHR
+//
+//  Created by Yanxin Yin on 4/21/18.
+//  Copyright © 2018 Yanxin Yin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SleepHR/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SleepHR
+//
+//  Created by Yanxin Yin on 4/21/18.
+//  Copyright © 2018 Yanxin Yin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
